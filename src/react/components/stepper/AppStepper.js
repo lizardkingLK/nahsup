a2c16a19c3a4bbe7ca5ce0d25c4987a8dfb72475
Sandeepa Fernando <chansanfdo1996@outlook.com>
2020-07-15 00:18:16 +0530

    sndp_minor_ui_fixes

--- src/react/components/stepper/AppStepper.js
+++ src/react/components/stepper/AppStepper.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import { KeyboardArrowLeft, KeyboardArrowRight } from '@material-ui/icons';
+import { MobileStepper, Paper, Typography, IconButton } from '@material-ui/core';
 
 const tutorialSteps = [
     {
